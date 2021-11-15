fbdb297028cd324695574cf3571bbb876fd46406
kimduc2003 <94033635+kimduc2003@users.noreply.github.com>
2021-11-15 10:01:30 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ function firstQuestion(){
 
  // switch button position
  function switchButton() {
-    var audio = new Audio('sound/sound.mp3');
+    var audio = new Audio('sound/duck.mp3');
     audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
